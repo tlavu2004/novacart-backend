8c75c1f47dc0aea5e0e8b2641c957b5745831088
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2026-06-04 01:39:29 +0700

    chore(project): initialize NovaCart project with Spring Boot and Maven wrapper

---
+++ src/main/java/com/tlavu/novacart/NovaCartApplication.java
@@ -0,0 +1,13 @@
+package com.tlavu.novacart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlavu/novacart/NovaCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlavu.novacart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
