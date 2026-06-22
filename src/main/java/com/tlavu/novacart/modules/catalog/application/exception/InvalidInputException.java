f44fd49e1c2f450f085fa4b89601ee87c1791a6e
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2026-06-23 04:55:36 +0700

    refactor(exception): rename ValidationException to InvalidInputException and update handler

--- src/main/java/com/tlavu/novacart/modules/catalog/application/exception/InvalidInputException.java
+++ src/main/java/com/tlavu/novacart/modules/catalog/application/exception/InvalidInputException.java
@@ -3,13 +3,15 @@ package com.tlavu.novacart.modules.catalog.application.exception;
 import com.tlavu.novacart.shared.exception.base.BaseException;
 import com.tlavu.novacart.shared.exception.code.ErrorCode;
 
-public class ValidationException extends BaseException {
+public class InvalidInputException extends BaseException {
 
-    public ValidationException(ErrorCode errorCode) {
+    public InvalidInputException(ErrorCode errorCode) {
+
         super(errorCode, errorCode.getDefaultMessage());
     }
 
-    public ValidationException(ErrorCode errorCode, String message) {
+    public InvalidInputException(ErrorCode errorCode, String message) {
+
         super(errorCode, message);
     }
 }
