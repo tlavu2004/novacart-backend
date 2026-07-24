2b8a11293b36d0f4b8cd93d833fb4becade0da02
Trương Lê Anh Vũ <truongleanhvu20052004@gmail.com>
2026-07-25 03:45:24 +0700

    feat(tests): introduce AbstractIntegrationTest for PostgreSQL integration testing

--- src/test/java/com/tlavu/novacart/NovaCartApplicationTests.java
+++ src/test/java/com/tlavu/novacart/NovaCartApplicationTests.java
@@ -1,10 +1,11 @@
 package com.tlavu.novacart;
 
+import com.tlavu.novacart.support.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NovaCartApplicationTests {
+class NovaCartApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
---
+++ src/test/java/com/tlavu/novacart/support/AbstractIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.tlavu.novacart.support;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+
+    @SuppressWarnings("resource")
+    @Container
+    static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>("postgres:17")
+            .withDatabaseName("novacart_test")
+            .withUsername("novacart_test")
+            .withPassword("novacart_test");
+
+    @DynamicPropertySource
+    static void configureDataSource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+}
